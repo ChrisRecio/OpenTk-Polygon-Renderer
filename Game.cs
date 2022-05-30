3b2c88b5335e3448bf0061c86a6e1588b9ed3c40
Chris <crecio12@gmail.com>
2022-05-30 18:36:17 -0400

    Drew a triangle for now

--- Game.cs
+++ Game.cs
@@ -7,9 +7,102 @@ namespace Collision_Simulation
 {
     internal class Game : GameWindow
     {
+        private int vertexBufferHandle, shaderProgramHandle, vertexArrayHandle;
+
+
         public Game() : base(GameWindowSettings.Default, NativeWindowSettings.Default)
         {
             this.CenterWindow(new Vector2i(1280, 768));
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, e.Width, e.Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnLoad()
+        {
+            GL.ClearColor(new Color4(0.5f, 0.5f, 0.5f, 1.0f));
+
+            float[] vertices = new float[]
+            {
+                0.0f, 0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+            };
+
+
+            this.vertexBufferHandle = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, this.vertexBufferHandle);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StreamDraw);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+            this.vertexArrayHandle = GL.GenVertexArray();
+            GL.BindVertexArray(vertexArrayHandle);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, this.vertexBufferHandle);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.BindVertexArray(0);
+
+            string vertexShaderCode =
+                @"
+                #version 330 core
+
+               layout (location = 0) in vec3 aPosition;
+                
+                void main()
+                {
+                    gl_Position = vec4(aPosition, 1f);
+                }
+                ";
+
+            string pixelShaderName =
+                @"
+                #version 330 core
+
+                out vec4 pixelColor;
+
+                void main()
+                {
+                    pixelColor = vec4(1.0f, 0.5f, 0.31f, 1.0f);
+                }
+                ";
+
+            int vertexShaderHandle = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShaderHandle, vertexShaderCode);
+            GL.CompileShader(vertexShaderHandle);
+
+            int pixelShaderHandle = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(pixelShaderHandle, pixelShaderName);
+            GL.CompileShader(pixelShaderHandle);
+
+            this.shaderProgramHandle = GL.CreateProgram();
+            GL.AttachShader(this.shaderProgramHandle, vertexShaderHandle);
+            GL.AttachShader(this.shaderProgramHandle, pixelShaderHandle);
+
+            GL.LinkProgram(this.shaderProgramHandle);
+
+            GL.DetachShader(this.shaderProgramHandle, vertexShaderHandle);
+            GL.DetachShader(this.shaderProgramHandle, pixelShaderHandle);
+
+            GL.DeleteShader(vertexShaderHandle);
+            GL.DeleteShader(pixelShaderHandle);
+
+            base.OnLoad();
+        }
+
+        protected override void OnUnload()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(this.vertexBufferHandle);
+
+            GL.UseProgram(0);
+            GL.DeleteProgram(this.shaderProgramHandle);
+
+            base.OnUnload();
         }
 
         // Called per frame update
@@ -21,8 +114,11 @@ namespace Collision_Simulation
         // Initial frame render
         protected override void OnRenderFrame(FrameEventArgs args)
         {
-            GL.ClearColor(new Color4(1.0f, 0.5f, 0.31f, 1.0f));
             GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.UseProgram(this.shaderProgramHandle);
+            GL.BindVertexArray(this.vertexArrayHandle);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
 
             this.Context.SwapBuffers();
             base.OnRenderFrame(args);
